f0f4a2946a52ee415d6a24f855f49ff11ad60d2f
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-12 20:48:14 -0700

    Update Bouncing-balls.js

--- Bouncing-balls.js
+++ Bouncing-balls.js
@@ -23,3 +23,9 @@ Examples:
 
 
 // Solution
+
+function bouncingBall(h,  bounce,  window) {
+  let rebounds = -1;
+  if (bounce > 0 && bounce < 1) while (h > window) rebounds+=2, h *= bounce;
+  return rebounds;
+}
